88f73bd50689e094eddec672a3d23e51e0df6fb9
marcosbrazz <marcos.brazz@gmail.com>
2018-01-28 19:32:23 -0200

    Corrigido formato do res.send

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var app = express();
 app.get('/check', function(req, res) {
 	console.log('Executing consorcio-bot manually');
 	consorciobot.start();
-	res.send('Done');
+	res.send({response:'done'});
 }) ;
 
 
